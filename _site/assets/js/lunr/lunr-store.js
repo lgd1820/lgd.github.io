2cfbd74910ccee76932c9abbc7b31f11d125f3ea
lgd1820 <lgd1810@gmail.com>
2021-12-03 13:41:30 +0900

    update blog

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "모의고사",
+        "excerpt":"문제 설명 수포자는 수학을 포기한 사람의 준말입니다. 수포자 삼인방은 모의고사에 수학 문제를 전부 찍으려 합니다. 수포자는 1번 문제부터 마지막 문제까지 다음과 같이 찍습니다. 1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, … 2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5,...","categories": ["programmers"],
+        "tags": ["programmers"],
+        "url": "/programmers/Practice-test/",
+        "teaser": null
+      }]
